649fd7b801240d00f2ad0863dc57b6348342b384
D4NNONY <56284773+D4NNONY@users.noreply.github.com>
2019-10-29 22:15:43 -0300

    Update exc 2.py

--- exc 2.py
+++ exc 2.py
@@ -4,3 +4,16 @@ Preencher uma lista de 8 elementos inteiros.
 Mostrar o vetor e informar quantos números são maiores que 30.
 Somar todos os números.
 '''
+lista5 = []
+n = 0
+for i in range(8):
+    x = int(input("Digite um valor inteiro:"))
+    lista5.append(x)
+
+lista5.sort()
+print(lista5[:])
+
+for c in range(8):
+    if (lista5[c]>30):
+        n = n + 1
+print ("Há {0} números maiores que 30.".format(n))
