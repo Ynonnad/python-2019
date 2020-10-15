ee1a129baa6ea80372958484f19fed5a83c492f9
Ynonnad <57160068+Ynonnad@users.noreply.github.com>
2020-10-15 16:39:00 -0300

    Update exc 11.py

--- exc 11.py
+++ exc 11.py
@@ -2,3 +2,11 @@
 exc 11
 Preencher uma lista com os números 10 a 20, e depois mostrar os elementos pares da lista de trás para frente.
 '''
+lista17 = []
+
+for i in range(10,21,2):
+    lista17.append(i)
+
+lista17.reverse()
+
+print(lista17[:])
