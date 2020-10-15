35f0153be7ce06e4d3cc806e8c7497cc4b0b4bbd
Matheus Cavalcanti <56284773+D4NNONY@users.noreply.github.com>
2020-10-15 15:18:58 -0300

    Create exc 9.py

---
+++ exc 9.py
@@ -0,0 +1,5 @@
+'''
+exc 99
+Neste exercício temos duas listas com 5 posições (0 a 4). Em cada lista entraremos com cinco números. 
+Mostrar os números e depois somar números que pertençam a mesma posição ou seja: [0]+[0], [1]+[1]
+'''
