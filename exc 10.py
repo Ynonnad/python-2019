85d2498bc10b869590e28e4c1436b049cb3676c2
Ynonnad <57160068+Ynonnad@users.noreply.github.com>
2020-10-15 16:46:06 -0300

    Update exc 10.py

--- exc 10.py
+++ exc 10.py
@@ -2,3 +2,14 @@
 exc 10
 Preencher uma lista com os números 10 a 20, e depois mostrar os elementos ímpares da lista.
 '''
+lista16 = []
+impares = []
+
+for i in range(10,21):
+    lista16.append(i)
+
+for i in range(10):
+    if (lista16[i]%2 != 0):
+        impares.append(lista16[i])
+
+print(impares[:])
