3b871587293089f520c5387df0e40608f73af589
Ynonnad <57160068+Ynonnad@users.noreply.github.com>
2020-10-15 16:34:34 -0300

    Update exc 12.py

--- exc 12.py
+++ exc 12.py
@@ -3,3 +3,23 @@ exc 12
 Preencher uma lista com os números 10 a 20, e depois mostrar os elementos pares da lista de trás para frente. 
 E também mostrar os números ímpares.
 '''
+
+lista18 = []
+lista19 = []
+
+for i in range(10,21,2):
+    lista18.append(i)
+
+for i in range(10,21):
+    if (i%2 != 0):
+        lista19.append(i)
+
+lista18.reverse()
+lista19.reverse()
+
+print('='*30)
+
+print('''
+nºs pares: {}
+nºs ímpares: {}
+'''.format(lista18[:],lista19[:])
